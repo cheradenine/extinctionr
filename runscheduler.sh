e02cbe921e7357028978a69239162774c5197fee
cheradenine <johnb.xr@pm.me>
2021-10-24 17:38:13 -0400

    fab and params to scheduler

--- runscheduler.sh
+++ runscheduler.sh
@@ -7,4 +7,5 @@ source /home/xr/xrboston.org/venv/bin/activate
 export DEBUG='false'
 export DJANGO_SETTINGS_MODULE=extinctionr.prod_settings
 
-python manage.py runreminders
+python manage.py runreminders --settings=extinctionr.prod_settings
+
